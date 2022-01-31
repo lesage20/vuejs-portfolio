3c53a1a22d099cc2cf90058e8616e3c8619102ad
lesage20 <angezanou00@gmail.com>
2022-01-31 14:51:03 +0000

    adding router and store

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
-  </div>
+  <v-app>
+    <!-- <v-app-bar transparent  >
+      <div class="d-flex align-center">
+        
+
+        
+      </div>
+
+      <v-spacer></v-spacer>
+    </v-app-bar> -->
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Vue from 'vue'
+Vue.mixin({
+  data: ()=>({
+    // authAPI: "http://127.0.0.1:8000/auth/",
+    authAPI: "http://192.168.43.108:8000/auth/"
+  })
+})
 
-#nav {
-  padding: 30px;
-}
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
+export default {
+  name: "App",
 
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  data: () => ({ }),
+};
+</script>
