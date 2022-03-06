bca015df9e600fe4e10f8ca0c2f5f93390c9a0c3
The-Ghost03 <75930919+The-Ghost03@users.noreply.github.com>
2022-03-06 16:16:13 +0000

    remis tout en place

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -3,4 +3,24 @@ import Vuetify from "vuetify/lib/framework";
 
 Vue.use(Vuetify);
 
-export default new Vuetify({});
+export default new Vuetify({
+  breakpoint: {
+    mobileBreakpoint: "sm", // This is equivalent to a value of 960
+  },
+  theme: {
+    themes: {
+      light: {
+        // le bleu du site
+        primary: "#1100af",
+        //lr rouge du site
+        secondary: "#E30613",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        //le vert de succes
+        success: "#00E676",
+        warning: "#FFC107",
+      },
+    },
+  },
+});
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,75 @@
 <template>
-<v-container :fluid="isMobile" class="grey lighten-3">
-    <h1>Dashboard</h1>
+  <div class="catalogue">
+    <SliderCarouselle />
 
-</v-container>
+    <v-alert dense class="mt-5" text icon="mdi-account-group" color="success">
+      <strong>Ressources employées</strong>
+    </v-alert>
+    <v-row class="mb-5 lg-px-5">
+      <v-col
+        lg="6"
+        md="6"
+        cols="12"
+        v-for="detail in ressources"
+        :key="detail.nom"
+      >
+        <v-card elevation="2" class="rounded-lg">
+          <v-card-text class="d-flex justify-space-between align-center">
+            <div class="">
+              <strong> {{ detail.nom }} </strong> <br />
+              <small>{{ detail.valeur }}</small>
+            </div>
+            <v-avatar v-if="detail.index == 2" size="100">
+              <v-img :src="detail.logo"></v-img>
+            </v-avatar>
+            <v-avatar v-else class="secondary" size="100">
+              <span
+                v-if="detail.index == 1"
+                class="white--text font-weight-medium"
+                >{{ detail.pourcentage }}% <br />
+                consommé</span
+              >
+              <span
+                v-if="detail.index == 3"
+                class="white--text font-weight-medium"
+                >effectif <br />
+                {{ detail.effectif }}</span
+              >
+            </v-avatar>
+          </v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
+    <v-alert
+      text
+      class="text-center mt-3 title"
+      icon="mdi-format-list-checks"
+      prominent
+      color="primary lighten-3"
+    >
+      Cahier de charges et avancée du projet
+    </v-alert>
+
+    <TimeLine />
+  </div>
 </template>
-
 <script>
-
+import SliderCarouselle from "@/components/SliderCarouselle.vue";
+import TimeLine from "@/components/TimeLine.vue";
 export default {
-    name: "Home",
-
-   
+  name: "Home",
+  components: { SliderCarouselle, TimeLine },
+  data: () => ({
+    ressources: [
+      { index: 1, nom: "Budget", valeur: "200 300 FCFA", pourcentage: "20" },
+      {
+        index: 2,
+        nom: "Chef de Projet",
+        valeur: "Koffi-BAT",
+        logo: "/images/Logo IEA-02.png",
+      },
+      { index: 3, nom: "Maitre d'oeuvre", valeur: "Bnet", effectif: "600" },
+    ],
+  }),
 };
 </script>
