3c8673b81bd188726454000b9c3086e826c7294c
The-Ghost03 <75930919+The-Ghost03@users.noreply.github.com>
2022-03-06 15:42:04 +0000

    installation et ajout de mes fichiers

---
+++ src/components/SliderCarouselle.vue
@@ -0,0 +1,45 @@
+<template>
+  <div>
+    <v-alert
+      text
+      class="text-center mt-3 title"
+      icon="mdi-image"
+      prominent
+      color="primary lighten-3"
+    >
+      Aperçu du chantier
+    </v-alert>
+    <v-carousel
+      class="my-1"
+      cycle
+      height="400"
+      hide-delimiter-background
+      show-arrows-on-hover
+    >
+      <v-carousel-item v-for="(slide, i) in slides" :key="i">
+        <v-sheet :color="colors[i]" height="100%">
+          <v-row class="fill-height" align="center" justify="center">
+            <div class="text-h2">{{ slide }} Slide</div>
+          </v-row>
+        </v-sheet>
+      </v-carousel-item>
+    </v-carousel>
+  </div>
+</template>
+<script>
+export default {
+  name: "SliderCarouselle",
+  data() {
+    return {
+      colors: [
+        "indigo",
+        "warning",
+        "pink darken-2",
+        "red lighten-1",
+        "deep-purple accent-4",
+      ],
+      slides: ["First", "Second", "Third", "Fourth", "Fifth"],
+    };
+  },
+};
+</script>
