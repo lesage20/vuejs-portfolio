3880d51de098151dcb446613aa21bbf80d35cbbc
The-Ghost03 <75930919+The-Ghost03@users.noreply.github.com>
2022-03-10 05:29:17 +0000

    about
    franchement j;etais pas tes inspiré ce matn

--- src/components/NotreAbout.vue
+++ src/components/NotreAbout.vue
@@ -1,55 +1,40 @@
 <template>
-  <v-card class="mx-auto" max-width="434" tile>
-    <v-img
-      height="100%"
-      src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg"
-    >
-      <v-row align="end" class="fill-height">
-        <v-col align-self="start" class="pa-0" cols="12">
-          <v-avatar class="profile" color="grey" size="164" tile>
-            <v-img
-              src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg"
-            ></v-img>
-          </v-avatar>
-        </v-col>
-        <v-col class="py-0">
-          <v-list-item color="rgba(0, 0, 0, .4)" dark>
-            <v-list-item-content>
-              <v-list-item-title class="text-h6">
-                Marcus Obrien
-              </v-list-item-title>
-              <v-list-item-subtitle>Network Engineer</v-list-item-subtitle>
-            </v-list-item-content>
-          </v-list-item>
-        </v-col>
-      </v-row>
-    </v-img>
-  </v-card>
+  <v-container>
+    <v-row justify="space-around">
+      <v-col
+        v-for="i in team"
+        :key="i.nom"
+        sm="12"
+        md="5"
+        class="pa-0"
+        cols="12"
+      >
+        <v-card max-width="344">
+            <v-img class="mx-auto" :max-width="100" :max-height="100" :src="i.photo"></v-img>
+<br/>
+          <v-card-title class="title accent--text">
+            {{ i.nom }}</v-card-title
+          >
+          <v-card-subtitle class="accent--text">{{ i.role }}</v-card-subtitle>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 <script>
 export default {
-    name:"NotreAbout",
+  name: "NotreAbout",
   data: () => ({
     team: [
       {
         nom: "Franck Schalôm",
-        Photo: "",
-        Role: "Dev Web Front-end",
+        photo: "/images/undraw_male_avatar_323b.svg",
+        role: "Dev Web Front-end",
       },
       {
-        nom: "Franck Schalôm",
-        Photo: "",
-        Role: "Dev Web Front-end",
-      },
-      {
-        nom: "Franck Schalôm",
-        Photo: "",
-        Role: "Dev Web Front-end",
-      },
-      {
-        nom: "Franck Schalôm",
-        Photo: "",
-        Role: "Dev Web Front-end",
+        nom: "Le Sage",
+        photo: "/images/undraw_female_avatar_w3jk.svg",
+        role: "Dev Back-end",
       },
     ],
   }),
